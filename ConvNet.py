3e843fd6da6d9dc00f67cbf4a868d4b624e96711
Akash <akash.darknight@gmail.com>
2018-03-31 10:03:00 +0530

    Application file

--- ConvNet.py
+++ ConvNet.py
@@ -359,4 +359,29 @@ dA, dW, db = conv_backward(Z, cache_conv)
 print("dA_mean =", np.mean(dA))
 print("dW_mean =", np.mean(dW))
 print("db_mean =", np.mean(db))
+def create_mask_from_window(x):
+    """
+    Creates a mask from an input matrix x, to identify the max entry of x.
+
+    Arguments:
+    x -- Array of shape (f, f)
+
+    Returns:
+    mask -- Array of the same shape as window, contains a True at the position corresponding to the max entry of x.
+    """
+
+    ### START CODE HERE ### (≈1 line)
+    mask = (x == np.max(x))
+    ### END CODE HERE ###
+
+    return mask
+
+
+# In[93]:
+
+np.random.seed(1)
+x = np.random.randn(2, 3)
+mask = create_mask_from_window(x)
+print('x = ', x)
+print("mask = ", mask)
 
